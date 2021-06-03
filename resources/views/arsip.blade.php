f9450e07574a1fd8eac77d87bdc908b69cd8f86d
Syafii2109 <siimam398@gmail.com>
2021-06-03 16:32:49 +0700

    revisi detail

--- resources/views/arsip.blade.php
+++ resources/views/arsip.blade.php
@@ -2,12 +2,56 @@
 
 @section('content')
 
-<div class=" vh-100 w-100 d-flex align-items-center justify-content-center" data-aos="fade-up">
-    <div class=" mt-5"></div>
-    <div class=" alert alert-info text-center m-5">
-        Halaman Arsip Masih Kosong
-        <a href="/pemenang"></a>
+<!-- Arsip -->
+<section class="arsip">
+    <div class="top-tittle" data-aos="fade-up" data-aos-easing="ease-in-out">
+        <h2>Arsip</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 arsip-box" data-aos="fade-up">
+                <a href="#">
+                    <div class="row align-items-center">
+                        <div class="col-1 icon">
+                            <img src="{{asset('/img/DTIK Fest.png')}}" alt="">
+                        </div>
+                        <div class="col text">
+                            <h4>DTIK Fest 2021</h4>
+                            <p>Politeknik Negeri Surabaya</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-12 arsip-box" data-aos="fade-up">
+                <a href="#">
+                    <div class="row align-items-center">
+                        <div class="col-1 icon">
+                            <img src="{{asset('/img/DTIK Fest.png')}}" alt="">
+                        </div>
+                        <div class="col text">
+                            <h4>DTIK Fest 2021</h4>
+                            <p>Politeknik Negeri Surabaya</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-12 arsip-box" data-aos="fade-up">
+                <a href="#">
+                    <div class="row align-items-center">
+                        <div class="col-1 icon">
+                            <img src="{{asset('/img/DTIK Fest.png')}}" alt="">
+                        </div>
+                        <div class="col text">
+                            <h4>DTIK Fest 2021</h4>
+                            <p>Politeknik Negeri Surabaya</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
+            
+<!-- End Arsip -->
 
 @endsection
